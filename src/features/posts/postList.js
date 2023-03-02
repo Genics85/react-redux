78976fee8990ba5a565093ec55c3c982b2db580c
unknown <eugeneamo85@gmail.com>
2023-03-02 21:57:49 +0000

    createAsyncThunk done

--- src/features/posts/postList.js
+++ src/features/posts/postList.js
@@ -1,50 +1,82 @@
-import {useState} from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-import { getAllPosts } from './postSlice';
-import {addPost} from "./postSlice";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  getAllPosts,
+  getPostStatus,
+  getPostError,
+  fetchPost,
+} from "./postSlice";
+import { addPost } from "./postSlice";
 import "./post.css";
 
-
 function PostList() {
-    const[title,setTitle]=useState("");
-    const[content,setContent]=useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-    const posts=useSelector(getAllPosts);
-    const dispatch=useDispatch();
+  const posts = useSelector(getAllPosts);
+  const postStatus=useSelector(getPostStatus)
+  const postError=useSelector(getPostError);
 
-    const defineTitle=(e)=>{
-        setTitle(e.target.value);
+  const dispatch = useDispatch();
+
+  const defineTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const defineContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmitForms = (e) => {
+    e.preventDefault();
+    if (title && content) {
+      dispatch(addPost({ author: title, content }));
     }
-    const defineContent=(e)=>{
-        setContent(e.target.value);
-    }
+  };
+ 
 
-    const onSubmitForms=(e)=>{
-        e.preventDefault();
-        if(title&&content){
-            dispatch(addPost({author:title,content}))
-        }
-    }
 
-    const renderedList=posts.map((post)=>{
-        return(
-            <article  key={post.id}>
-                <h3>{post.author}</h3>
-                <p>{post.content.substring(0,40)}</p>
-            </article>
-        )
-    })
+  useEffect(() => {
+    if(postStatus==="idle"){
+      dispatch(fetchPost());
+    }
+  }, [postStatus, dispatch]);
+
+  let value;
+  if(postStatus==="loading"){
+    value=<p> "loading..."</p>;
+  }else if(postStatus==="succeeded"){
+    value=posts.map((post) => {
+      return (
+        <article key={post.id}>
+          <h3>{post.author}</h3>
+          <p>{post.body.substring(0, 40)}</p>
+        </article>
+      );
+    });
+  }else if(postStatus==="failed"){
+    value=<p>{postError}</p>
+  }
 
   return (
     <section className="post-screen">
-        <form className="post-form" onSubmit={onSubmitForms}>
-            <input onChange={defineTitle} value={title} type='text' placeholder='Enter title' />
-            <input onChange={defineContent} value={content} type='text' placeholder='Content goes here'/>
-            <button type='submit'> Add to posts</button>
-        </form>
-        {renderedList}
+      <form className="post-form" onSubmit={onSubmitForms}>
+        <input
+          onChange={defineTitle}
+          value={title}
+          type="text"
+          placeholder="Enter title"
+        />
+        <input
+          onChange={defineContent}
+          value={content}
+          type="text"
+          placeholder="Content goes here"
+        />
+        <button type="submit"> Add to posts</button>
+      </form>
+      {value}
     </section>
-  )
+  );
 }
 
-export default PostList
+export default PostList;
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,45 +1,51 @@
-import { createSlice,nanoid,createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const BASE_URL="https://jsonplaceholder.typicode.com/posts";
+const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
 
-export const fetchPost=createAsyncThunk("fetch/post", async()=>{
-    const response= await axios.get(BASE_URL);
-    return [...response.data];
-})
-
-const initialState=[
-    {
-    id:1,
-    content:"Reprehenderit fugiat velit irure proident.",
-    author:"Genics",
-    credit:"something nice",
-    year:2023
-},
-    {
-    id:2,
-    content:"Reprehenderit fugiat velit irure proident.",
-    author:"Yaro",
-    credit:"something nice",
-    year:2023
-},
-]
-
-export const postSlice=createSlice({
-    name:"posts",
-    initialState,
-    reducers:{
-        addPost:{
-            reducer(state,action){
-            state.push({...action.payload,id:nanoid()});},
-            
-            extraReducers(builder){
-                builder.addCase()
-            }
-        }
-    }
+export const fetchPost = createAsyncThunk("posts/fetchPost", async () => {
+  const response = await axios.get(BASE_URL);
+  console.log(response.data);
+  return response.data;
 });
 
-export const getAllPosts=(state)=> state.posts;
-export const {addPost}=postSlice.actions;
+const initialState = {
+  posts: [],
+  status: "idle",
+  error: null,
+};
+
+export const postSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    addPost: {
+      reducer(state, action) {
+        state.posts.push({ ...action.payload, id: nanoid() });
+      },
+  
+    },
+  },
+  extraReducers(builder){
+      builder
+        .addCase(fetchPost.pending, (state, action) => {
+          state.status = "loading";
+        })
+        .addCase(fetchPost.fulfilled, (state, action) => {
+          console.log("inside the succeeded");
+          state.status = "succeeded";
+          const loadedPosts = action.payload;
+          state.posts = state.posts.concat(loadedPosts);
+        })
+        .addCase(fetchPost.rejected, (state, action) => {
+          state.status = "failed";
+          state.error = action.error.message;
+        });
+    },
+});
+
+export const getAllPosts = (state) => state.posts.posts;
+export const getPostStatus = (state) => state.posts.status;
+export const getPostError = (state) => state.posts.error;
+export const { addPost } = postSlice.actions;
 export default postSlice.reducer;
